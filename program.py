7db066aa038a90383e8703bd3ace106b03b17296
Jisa <69690678+Jisaalapatt@users.noreply.github.com>
2025-01-03 23:41:57 -0800

    Add files via upload

---
+++ program.py
@@ -0,0 +1,11 @@
+# program.py
+sum=1+2
+print(sum)
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+planets_in_solar_system = 8 # int, pluto used to be the 9th planet, but is too small
+distance_to_alpha_centauri = 4.367 # float, lightyears
+can_liftoff = True
+shuttle_landed_on_the_moon = "Apollo 11" #string
+type(distance_to_alpha_centauri) ## <class 'float'>
